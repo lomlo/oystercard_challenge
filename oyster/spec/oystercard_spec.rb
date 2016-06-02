c440c16848b33aec5a300686a35a6930ffb52c78
lomlo <michael.lo@live.co.uk>
2016-06-02 09:32:20 +0100

    refactor rspec tests

--- oyster/spec/oystercard_spec.rb
+++ oyster/spec/oystercard_spec.rb
@@ -10,13 +10,9 @@ describe Oystercard do
 
   describe "responds to" do
     it { is_expected.to respond_to(:balance) }
-
     it { is_expected.to respond_to(:top_up).with(1).argument }
-
     it { is_expected.to respond_to(:touch_in).with(1).argument }
-
     it { is_expected.to respond_to(:touch_out) }
-
     it { is_expected.to respond_to(:entry_station) }
   end
 
@@ -27,18 +23,15 @@ describe Oystercard do
   end
 
 	describe "#top_up" do
-
 		it "should increase the balance" do
-			expect(card.top_up(5)).to be == card.balance
+			expect(card.top_up(5)).to eq card.balance
 		end
-
 		it 'raises an error if top_up amount would push balance over 90' do
 			expect { card.top_up(91) }.to raise_error "Can't add to your balance; would breach the £90 limit"
 		end
 	end
 
   describe "#touch_in" do
-
     it "changes the card to be in_journey" do
       card.top_up(1)
       card.touch_in(entry_station)
@@ -55,31 +48,29 @@ describe Oystercard do
       card.touch_in(entry_station)
     end
     it "changes the card to not be in_journey" do
-     card.touch_out(exit_station)
-     expect(card).not_to be_in_journey
+      card.touch_out(exit_station)
+      expect(card).not_to be_in_journey
    end
    it "calls the #deduct method" do
-     expect(card).to receive(:deduct)
-     card.touch_out(exit_station)
+      expect(card).to receive(:deduct)
+      card.touch_out(exit_station)
    end
 
    it "should deduct 1 from the balance" do
-     expect {card.touch_out(exit_station)}.to change{card.balance}.by(-1)
+      expect {card.touch_out(exit_station)}.to change{card.balance}.by(-1)
    end
 
    it "should forget the journey's entry station" do
-     card.touch_out(exit_station)
-     expect(card.entry_station).to eq nil
+      card.touch_out(exit_station)
+      expect(card.entry_station).to eq nil
    end
 
   end
 
   describe "#in_journey?" do
-
     it "returns false if not in journey" do
       expect(card.in_journey?).to eq false
     end
-
     it "returns true if it is in journey" do
       card.top_up(1)
       card.touch_in(entry_station)
@@ -88,23 +79,28 @@ describe Oystercard do
   end
 
   describe "#journeys" do
+    before(:each) do
+      card.top_up(2)
+    end
     it "shows all the trips that have been made with the card" do
       expect(card.journeys).to be_a Array
     end
     it "should start with no journeys" do
-      expect(card.journeys).to eq []
+      expect(card.journeys).to be_empty
     end
-    it "contains the entry and exit station" do
-      card.top_up(10)
+    context "records a pair of entry and exit stations" do
       card.touch_in('station1')
       card.touch_out('station2')
       expect(card.journeys).to include('station1' => 'station2')
+    end
+    it "records multiple journeys" do
+      card.touch_in('station1')
+      card.touch_out('station2')
       card.touch_in('station3')
       card.touch_out('station4')
       expect(card.journeys).to include('station3' => 'station4')
       expect(card.journeys).to include('station1' => 'station2')
     end
-
   end
 
 end
