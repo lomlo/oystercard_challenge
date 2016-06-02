ba0b40703ef0c429e5c09a3e3bcefe7400d2d1c4
Rb2030 <rossmail2050@gmail.com>
2016-06-02 16:06:35 +0100

    In Journey, but still playing with deduct

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,28 @@
 class Oystercard
   attr_reader :balance
 
+MAXIMUM = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up(money)
-    @balance += money
+    fail "Cannot top up more than 90" if balance + money > MAXIMUM
+      @balance += money
+  end
+
+  def deduct(fare)
+    @balance -= fare
+  end
+
+  def touch_in
+  end
+
+  def touch_out
+  end
+
+  def in_journey
   end
 end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,6 +2,8 @@ require 'oystercard'
 
 describe Oystercard do
   subject(:card) { described_class.new }
+
+
   describe '#balance' do
     it "oystercard understands the balance method" do
       expect(card).to respond_to(:balance)
@@ -13,6 +15,10 @@ describe Oystercard do
       card.top_up(1)
       expect(card.balance).to eq 1
     end
+    it 'has a maximum balance of 90' do
+      expect(card.top_up(90)).to eq card.balance
+    end
+
   end
 
   describe '#top_up' do
@@ -23,5 +29,42 @@ describe Oystercard do
       card.top_up(1)
       expect(card.top_up(3)).to eq 4
     end
+     it 'raises error when exceeding maximum limit' do
+      expect{card.top_up(91)}.to raise_error "Cannot top up more than 90"
+    end
   end
+
+  describe '#deduct' do
+    it 'deducts 5 from the balance' do
+      p "#{card.balance}"
+      card.deduct(5)
+      p "#{card.deduct(5)}"
+      p "#{card.balance}"
+      expect(card.balance).to eq -5
+      p 'flang'
+    end
+    it 'deducts 10 from the balance' do
+      card.deduct(-10)
+      expect(card.balance).to eq -10
+    end
+  end
+
+  describe '#touch_in' do
+    it 'The card is touched into a station' do
+      expect(card).to respond_to(:touch_in)
+    end
+  end
+
+  describe '#touch_out' do
+    it 'the card has left a station' do
+      expect(card).to respond_to(:touch_out)
+    end
+  end
+
+  describe '#in_journey' do
+    it 'the card is on a journey' do
+      expect(card).to respond_to(:in_journey)
+    end
+  end
+
 end
